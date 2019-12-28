4c2594f763af95184da00c3eabffb04686d15e37
Malik Mahmud <almalikmahmud@gmail.com>
2019-12-28 16:31:12 +0100

    Migrate models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+  task = models.CharField(max_length=100)
+  pub_date = models.DateTimeField("date published")
+  def __str__(self):
+    return self.task
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from .models import Todo
+
 
 def index(request):
-  return render(request, "index.html", {})
+  if request.method == 'POST':
+    task = request.POST['task']
+    if task:
+      Todo.objects.create(task=task, pub_date=timezone.now())
+    return HttpResponseRedirect('/')
+  else:
+    todos = Todo.objects.all()
+    return render(request, "index.html", {'todos': todos })
 
+def detail(request, id):
+  if request.method == 'POST':
+    Todo.objects.get(id=id).delete()
+  
+  return HttpResponseRedirect("/")
+  
